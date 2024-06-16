13630140e22d979d53871e2091a6c7f0541f7c43
Keltars <keltars2000@gmail.com>
2024-06-16 12:40:58 +0300

    task 4

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import {
 import article from './data/article.json';
 import stats from './data/stats.json';
 import forbes from './data/forbes.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
@@ -34,7 +35,7 @@ export const App = () => {
         <ForbesList forbes={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory />
+        <CryptoHistory transactions={transactions} />
       </Container>
     </Section>
   );
--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,31 @@
-// const tableHeaders = ['№', 'price', 'amount', 'date'];
+import { formatDate } from 'helpers/date';
+import style from './CryptoHistory.module.css';
 
-export const CryptoHistory = () => {
-  return <h2>CryptoHistory</h2>;
+const tableHeaders = ['№', 'price', 'amount', 'date'];
+
+export const CryptoHistory = ({ transactions }) => {
+  return (
+    <table className={style.table}>
+      <thead className={style.thead}>
+        <tr>
+          {tableHeaders.map(el => (
+            <th key={el} className={style.th}>
+              {el}
+            </th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map((el, i) => (
+          <tr key={el.id} className={style.tr}>
+            <td className={style.td}>{i + 1}</td>
+            <td className={style.td}>{el.price}</td>
+            <td className={style.td}>{el.amount}</td>
+            <td className={style.td}>{formatDate(el.date)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
