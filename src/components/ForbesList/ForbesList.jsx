acac10408bee0f84918d4550184f1a338ecca060
RomaBez <bezvuglyak.roman@gmail.com>
2024-06-16 12:12:06 +0300

    task 3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,3 +1,30 @@
-export const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import style from './ForbesList.module.css';
+import { ForbesListItem } from '../ForbesListItem/ForbesListItem';
+
+export const ForbesList = ({ forbes }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {forbes.map(forbeEl => {
+          return (
+            <li key={forbeEl.id} className={style.item}>
+              <ForbesListItem
+                name={forbeEl.name}
+                capital={forbeEl.capital}
+                avatar={forbeEl.avatar}
+                isIncrease={forbeEl.isIncrease}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
